397ded1cb734434e3b9ef934067359becb29d9a1
aceeedev <cyberandrew03@gmail.com>
2023-12-15 22:24:32 -0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '../views/MainPage.vue'
+import PageNotFound from '../views/PageNotFound.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: MainPage
+  },
+  {
+    path: '/:pathMatch(.*)',
+    component: PageNotFound,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
---
+++ src/views/MainPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>Hello</h1>
+
+    <button @click="sendData">send</button>
+</template>
+  
+  
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'Mainpage',
+
+        methods: {
+            async sendData() {
+                try {
+                    const response = await axios.get('https://v519b0wsek.execute-api.us-west-1.amazonaws.com/beta/login');
+                    // JSON responses are automatically parsed.
+                    console.log(response.data)
+                } catch (error) {
+                    console.log(error);
+                }
+            
+
+                try {
+                    const response = await axios.get('https://v519b0wsek.execute-api.us-west-1.amazonaws.com/beta/login');
+                    // JSON responses are automatically parsed.
+                    console.log(response.data)
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+    }
+</script>
+
+
+<style>
+
+</style>
