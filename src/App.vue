1af109fff5bc561947976a83e39afb9f0ea1b47e
aceeedev <cyberandrew03@gmail.com>
2023-12-25 16:25:13 -0800

    Updated html and css

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,57 @@ export default {
 }
 
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+  html {
+    height: 100%;
+  }
+
+  body {
+    height: 100%;
+    margin: 0px;
+    font-family: 'Inter';
+    font-weight: 300;
+    font-size: 1rem;
+    color: white;
+
+    background-repeat: no-repeat;
+    background-image: linear-gradient(45deg, #121212, 90%, #0f7031);
+
+    --secondary: #004DA6;
+    --background: white;
+    --background2: #E3E9EF;
+    --accent: #c6cbf4;
+  }
+
+  h1 {
+    font-size: 6rem;
+    font-weight: 900;
+    margin: 0;
+    line-height: 1.15;
+  }
+
+  h2 {
+    font-size: 3.5rem;
+    font-weight: 500;
+    margin: 5px 0 5px;
+  }
+
+  h3 {
+    font-size: 1.75rem;
+    font-weight: 400;
+  }
+
+  h4 {
+    font-size: 1.125rem;
+    font-weight: 400;
+    margin: 0;
+  }
+
+  p {
+    line-height: 1.85;
+    font-size: 1.1rem;
+  }
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,21 +1,34 @@
 <template>
-    <div>
-        <h1>Hello</h1>
+    <div style="margin-left: 10%; margin-right: 10%;"> 
+        <h1>Zodiac<br>Analyzer</h1>
+        <p>
+            Unlock the power of your musical stars! Your musical journey is written in the stars, 
+            and by sharing your favorite tunes, you'll help discover the cosmic connections between your 
+            zodiac sign and the beats that move you. Don't miss out on this unique opportunity to explore 
+            the harmony between your zodiac sign and your favorite songs. Submitting your Spotify data is 
+            completely anonymous and helps find the connection between zodiac signs and music.
+        </p>
 
-        <select name="Month" @change="onMonthChange($event)" class="form-control" v-model="key">
-            <option v-for="(item, index) in ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']" v-bind:value="index + 1" v-bind:key="item">
-                {{ item }}
-            </option>
-        </select>
-        <select name="Day" @change="onDayChange($event)" class="form-control" v-model="key">
-            <option v-for="index in 31" v-bind:value="index" v-bind:key="index">
-                {{ index }}
-            </option>
-        </select>
-
-        <button @click="calculateZodiacSign">What's my Zodiac Sign</button>
-
-        <button @click="redirect">Send</button>
+        <div>
+            <p>Please enter your birth date:</p>
+            <label for="Month">Month: </label>
+            <select name="Month" @change="onMonthChange($event)" class="form-control" v-model="key">
+                <option v-for="(item, index) in ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']" v-bind:value="index + 1" v-bind:key="item">
+                    {{ item }}
+                </option>
+            </select>
+            <br>
+            <label for="Day">Day: </label>
+            <select name="Day" @change="onDayChange($event)" class="form-control" v-model="key">
+                <option v-for="index in 31" v-bind:value="index" v-bind:key="index">
+                    {{ index }}
+                </option>
+            </select>
+        </div>
+        
+        <br>
+        <button @click="redirect" style="border-radius: 12px; padding: 12px 20px;">Send</button>
+        <p style="color: red">{{ error }}</p>
     </div>
 </template>
   
@@ -51,6 +64,12 @@
 
     export default {
         name: 'Mainpage',
+
+        data() { 
+            return {
+                error: '' 
+            }
+        },
 
         setup() {
             const route = useRoute()
@@ -93,6 +112,13 @@
                 saveStorage('day', event.target.value)
             },
             async redirect() {
+                const month = getStorage('month');
+                const day = getStorage('day');
+                
+                if (month === null || day === null) {
+                    this.error = 'Please enter your birth date'
+                    return;
+                }
                 window.location.href='https://accounts.spotify.com/authorize?response_type=code&client_id=280ec2cdd2bd49549c49ee81a7017af3&redirect_uri=https%3A%2F%2Fspotify-zodiac.netlify.app&scope=user-read-private%20user-read-email%20user-top-read'
             },
         }
